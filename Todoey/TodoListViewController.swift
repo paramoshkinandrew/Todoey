f1c43033c0a08e26eb8aca3d00cc5861ef6259c3
Andrew Paramoshkin <paramoshkin.andrew@gmail.com>
2018-06-02 15:12:06 +0300

    Create a dummy UITableView with hardcoded stubs

---
+++ Todoey/TodoListViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Andrew Paramoshkin on 02/06/2018.
+//  Copyright © 2018 Andrew Paramoshkin. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    
+    let itemArray = ["First", "Second", "Third"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    // MARK: - UITableView Datasource methods
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+}
+
